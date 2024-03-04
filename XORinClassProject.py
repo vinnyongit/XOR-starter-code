5e2da5595a6f01bd4b93fa5cb3c963e44f454c25
vinnyongit <144164943+vinnyongit@users.noreply.github.com>
2024-03-04 11:59:02 -0800

    Update XORinClassProject.py

--- XORinClassProject.py
+++ XORinClassProject.py
@@ -1,5 +1,4 @@
-import math
-
+import math 
 characters = [
     # lowercase characters
 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
@@ -20,64 +19,78 @@ def decode(binary):
     return characters[charIndex]
 
 
-def XOR(bit1, bit2):
 
+def XOR (bit1, bit2):
     if bit1 == bit2:
-        return '0'
+        return "0"
     else:
-        return '1'
+        return "1"
 
-def XORonByte(byte, key):
 
-    emsg = ""
+
+def XORonByte(byte,key):
+
+    ebyte = ""
 
     for i in range(len(byte)):
-        emsg +=  XOR(byte[i], key[i])
-    
-    return emsg
-
-#print(XORonByte('10101', '00101'))
+        ebyte += XOR(byte[i], key[i])
+    return ebyte
 
 def XORonLetter(letter, keyLetter):
+    binaryLetter = encode(letter)
+    binaryKeyLetter = encode(keyLetter)
 
-    letterBinary = encode(letter)
-    keyLetterBinary = encode(keyLetter)
-
-    encryptedLetter = XORonByte(letterBinary, keyLetterBinary)
+    encryptedLetter = XORonByte(binaryLetter, binaryKeyLetter)
 
     return decode(encryptedLetter)
 
 def XORonSentence(sentence, key):
-
-    encryptedSentence = ""
+    
+    esentence = ""
     genKey = generateKey(sentence, key)
 
     for i in range(len(sentence)):
+        esentence += XORonLetter(sentence[i], genKey[i])
 
-        encryptedSentence += XORonLetter(sentence[i], genKey[i])
+    return esentence
 
-    return encryptedSentence
+
+
 
 def generateKey(message, key):
 
-    if len(message) == len(key):
+    
+    if (len(message) == len(key)):
         return key
+
     elif len(message) < len(key):
-        return key[0:len(message)]
+
+        return key [0:len(message)]
+
     else:
         tempKey = ""
+
         repititions = math.floor(len(message)/len(key))
         remainder = len(message) % len(key)
 
         for i in range(repititions):
             tempKey += key
-
+        
         tempKey += key[0:remainder]
 
         return tempKey
-    
 
-message = input("Please enter your message that you either want to encrypt or decrypt. ")
-key = input("Please enter the key. ")
 
-print("Here is your encrypted/decrypted message: ", XORonSentence(message, key))
+message= input("Please enter your message that you either want to encrypt or decrypt")
+key = input("Please enter the key")
+
+print("Here is your encrypted/decryped message: ", XORonSentence(message,key))
+
+print(XORonSentence("hello", "world"))
+print(XORonSentence("rkAan", "world"))
+print(XORonSentence("&avrvLYpjgiWtmewbSfq bl",  "Beaver believers leave"))
+
+
+
+print(XOR("0","0"))
+print(XORonByte("000", "101"))
